a90edc96f6bfa9bbca468880f50e47fbef121573
User <User>
2016-09-05 20:56:33 +0300

    first commit

---
+++ Проект1/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+class lab1{
+
+	public:
+		int j;
+
+		void method()  
+		{
+			cout << "Skolko let ?"<< endl;
+			cin >> j;
+			cout << "tebe " << j << " let"<< endl;
+	    }
+
+
+};
+
+
+void main()
+{
+	lab1 first_Object;
+	first_Object.method();
+
+
+system("pause");
+}
